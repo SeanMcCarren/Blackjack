7e7f92c61d20e1658b5cc5ce0317f640ea7560c0
chickenstrips <mccarrensean@gmail.com>
2018-11-28 12:20:23 +0100

    fix and test of stack

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Stack stack = new Stack(6); //Init a stack of 6 decks of cards
-        stack.pullRandom(); 
-
-        DealerAI player1 = new DealerAI();
-        player1.pulled(16);
-        System.out.println("Player wants?: " + player1.wantsNext());
-        player1.pulled(1);
-        System.out.println("Player wants?: " + player1.wantsNext());
+        Stack stack = new Stack(6);
+        while (stack.isNext()) {
+            System.out.println(stack.pull());
+        }
     }
 }
