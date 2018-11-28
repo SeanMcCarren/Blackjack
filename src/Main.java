eeab36410f30a9737e6ffbba188614a419079165
chickenstrips <mccarrensean@gmail.com>
2018-11-28 12:25:48 +0100

    asdf

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        Stack stack = new Stack(6);
-        while (stack.isNext()) {
-            System.out.println(stack.pull());
-        }
     }
 }
