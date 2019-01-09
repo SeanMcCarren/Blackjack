e92dcfc029a17979ea01cde40531f2a23b7090fe
chickenstrips <mccarrensean@gmail.com>
2019-01-09 19:27:55 +0100

    ilhasd;jasdf

--- src/StrategyGenerator.java
+++ src/StrategyGenerator.java
@@ -1,33 +1,139 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class StrategyGenerator {
 
-    private static double[] valueProbability;
-    private static int dealerUpperLimit = 17;
+    private double[] valueProbability;
+    private int dealerUpperLimit = 17;
+    private List<List<Boolean>> takeListHard;
+    private List<List<Boolean>> takeListSoft;
+    
 
     public static void main(String[] args) {
 
-        getValueProbabilityDealer();
+        new StrategyGenerator().runFirstCardDependent();
 
-        printArray(valueProbability);
-
-        APHD root = new APHD();
-        root.probability = 1.0;
-        traverse(root, 21); //to create all options
-        bestMethodTraversal(root); //we should ignore method suggestions if depth <= 2, but i dont do that here
-
-        //root.options.get(4).options.get(6).print(new ArrayList<Integer>() {{add(6); add(8);}}, "");
-
-        System.out.println(root.expOutcomeBestMethod);
     }
 
-    private static void printArray(double[] arr) {
-        for ( int i = 0; i < arr.length; i++ ) {
-            System.out.println(i + ":\t" + arr[i]);
+    public void test() {
+        getValueProbabilityDealer();
+        APHD node = new APHD();
+        node.value = 21;
+        node.depth = 2;
+        //System.out.println(expectedOutcomeNoTake(node));
+        //printArray(valueProbability);
+    }
+
+    public void runFirstCardDependent() {
+        //of course the dealer has one card open. Say we want to generate a strategy based on that too..
+        for (int dealerFirstCard = 2; dealerFirstCard <= 11; dealerFirstCard++) {
+            System.out.print("Starting calculation of strategy table given the dealer's open card is of value " + Integer.toString(dealerFirstCard));
+            APHD root = new APHD();
+            root.value = dealerFirstCard;
+            root.probability = 1.0;
+            root.depth = 1;
+            if (dealerFirstCard != 11) {
+                root.aces = 1;
+            } else {
+                root.aces = 0;
+            }
+            traverse(root, dealerUpperLimit);
+            valueProbability = getValueProbability(root, true);
+            System.out.println(". The probability table of dealer: ");
+            printArray(valueProbability);
+            run();
         }
     }
 
-    private static double probabilityDeathGivenPathTakeOne(APHD obj) {
+    public void run() {
+        if (valueProbability == null) {
+            getValueProbabilityDealer();
+        }
+        APHD root = new APHD();
+        root.probability = 1.0;
+        root.depth = 0;
+        traverse(root, 21); //to create all options
+        bestMethodTraversal(root); //we should ignore method suggestions if depth <= 2, but i dont do that here
+        //root.options.get(8).options.get(9).print(new ArrayList<Integer>() {{add(10); add(11);}}, "");
+
+        takeListHard = new ArrayList<List<Boolean>>();
+        takeListSoft = new ArrayList<List<Boolean>>();
+        for (int value = 0; value < 21; value ++) { //consider having values 0..20
+            takeListHard.add(new ArrayList<Boolean>());
+            takeListSoft.add(new ArrayList<Boolean>());
+            
+        }
+        traverseTake(root); //list all take booleans per value of node
+        for (int i = 2; i < 21; i++) {
+            //check all take values for if they are equal!
+            if(!takeListSoft.get(i).isEmpty()) {
+                boolean bPrev = takeListSoft.get(i).get(0);
+                for (boolean b : takeListSoft.get(i)) {
+                    if (bPrev != b) {
+                        System.out.println("PROBLEM SOFT");
+                        //for (boolean b2 : takeListSoft.get(i)) {
+                        //    System.out.print(b2?"1":"0");
+                        //}
+                        return;
+                    }
+                }
+            }
+            if(!takeListHard.get(i).isEmpty()) {
+                boolean bPrev = takeListHard.get(i).get(0);
+                for (boolean b : takeListHard.get(i)) {
+                    if (bPrev != b) {
+                        System.out.println("PROBLEM HARD");
+                        //for (boolean b2 : takeListHard.get(i)) {
+                        //    System.out.print(b2?"1":"0");
+                        //}
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("V  S H (value, soft advice, hard advice)");
+        for (int i = 4; i < 21; i++) {
+            System.out.print(Integer.toString(i) + (i<10?" ":"") + " ");
+            if(!takeListSoft.get(i).isEmpty()) {
+                System.out.print(takeListSoft.get(i).get(0)?"1":"0");
+            } else {
+                System.out.print(" ");
+            }
+            System.out.print(" ");
+            if(!takeListHard.get(i).isEmpty()) {
+                System.out.print(takeListHard.get(i).get(0)?"1":"0");
+            } else {
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+        //System.out.println(root.expOutcomeBestMethod);
+    }
+
+    private void traverseTake(APHD root) {
+        if (root.value < 21) {
+            if (root.value > 1) {
+                //System.out.println(Integer.toString(root.value) + " " + Integer.toString(root.aces) + " " + (root.bestOptionIsTake ? "take":"dont"));
+                if (root.aces == 0) {
+                    takeListHard.get(root.value).add(root.bestOptionIsTake);
+                } else {
+                    takeListSoft.get(root.value).add(root.bestOptionIsTake);
+                }
+            }
+            for (APHD child : root.options) {
+                traverseTake(child);
+            }
+        }
+    }
+
+
+    private void printArray(double[] arr) {
+        for ( int i = 0; i < arr.length; i++ ) {
+            System.out.println(/*i + ":\t" + */arr[i]);
+        }
+    }
+
+    private double probabilityDeathGivenPathTakeOne(APHD obj) {
         double prob = obj.deathProbability;
         for (APHD child : obj.options) {
             prob -= child.deathProbability;
@@ -35,7 +141,7 @@ public class StrategyGenerator {
         return prob/obj.probability;
     }
 
-    private static double sum(double[] arr) {
+    private double sum(double[] arr) {
         double result = 0;
         for (double val : arr) {
             result += val;
@@ -43,9 +149,9 @@ public class StrategyGenerator {
         return result;
     }
 
-    private static void bestMethodTraversal(APHD node) {
+    private void bestMethodTraversal(APHD node) {
         double expOutcomeNoTake = expectedOutcomeNoTake(node);
-        if (node.options != null) {
+        if (node.options != null && !node.options.isEmpty()) {
             double expOutcomeTake = 0.0;
             for (APHD child : node.options) {
                 //assume we recursively take the best option.
@@ -66,12 +172,15 @@ public class StrategyGenerator {
         }
     }
 
-    private static double expectedOutcomeNoTake(APHD node) {
-        return valueProbability[22] + dealerOddsCumulative(node.value - 1)
-                - (dealerOddsCumulative(21) - dealerOddsCumulative(node.value + 1));
+    private double expectedOutcomeNoTake(APHD node) {
+        if (node.depth == 2 && node.value == 21) {
+            return 1.5 * (1-valueProbability[0]);
+        }
+        return (valueProbability[22] + dealerOddsCumulative(node.value - 1)
+                - (dealerOddsCumulative(21) - dealerOddsCumulative(node.value + 1)));
     }
 
-    private static void getVPHelper(APHD root, int depth, double[] storage) {
+    private void getVPHelper(APHD root, int depth, double[] storage) {
         if (root.options.size() > 0) {
             for (APHD child : root.options) {
                 getVPHelper(child, depth+1, storage);
@@ -86,7 +195,7 @@ public class StrategyGenerator {
         }
     }
 
-    private static double dealerOddsCumulative(int x) {
+    private double dealerOddsCumulative(int x) {
         if (valueProbability == null) {
             getValueProbabilityDealer();
         }
@@ -94,14 +203,14 @@ public class StrategyGenerator {
         for (int i = dealerUpperLimit; i <= (x > 21 ? 21 : x); i++) {
             cumulative += valueProbability[i];
         }
-        if (x == 21) {
+        if (x >= 21) {
             cumulative += valueProbability[0];
         }
         return cumulative;
     }
 
     // firstCard = true implies the root is already a chosen card
-    public static double[] getValueProbability(APHD root, boolean firstCard) {
+    public double[] getValueProbability(APHD root, boolean firstCard) {
         //result[0] is odd of blackjack
         //result[i] is odd of ending with i (of course you dont end with ...)
         //result[22] is death probability
@@ -111,44 +220,46 @@ public class StrategyGenerator {
         return result;
     }
 
-    public static void getValueProbabilityDealer() {
+    public void getValueProbabilityDealer() {
         APHD root = new APHD();
         root.probability = 1.0;
+        root.depth = 0;
         traverse(root, dealerUpperLimit);
         valueProbability = getValueProbability(root, false);
     }
   
     // passing limit = 17 yields dealer strat. limit = 21 yields always play strat
-    public static void traverse(APHD par, int lim) {
+    public void traverse(APHD par, int lim) {
         if (par.value >= lim) {
             return;
         }
         for (int i = 2; i <= 11; i++) {
             // aka: not: go over && hard hand && i != 11
-            if (par.value + i <= 21 || par.soft || i == 11) {
+            if (par.value + i <= 21 || par.aces > 0 || i == 11) {
                 
                 APHD opt = new APHD();
                 opt.latestCard = i;
                 opt.parent = par;
+                opt.depth = par.depth +1;
                 opt.probability = par.probability * (i==10?4.0:1.0) / 13.0;
                 opt.value = par.value;
+                opt.aces = par.aces;
 
                 // dealing with aces can be tricky
 
                 // stay under 21, no ace
                 if (opt.value + i <= 21 && i != 11) {
                     opt.value += i;
-                    opt.soft = par.soft;
                 //go over 21 but you do have a soft hand
-                } else if (opt.value + i > 21 && par.soft) {
+                } else if (opt.value + i > 21 && par.aces>0) {
                     opt.value -= 10;
-                    opt.soft = false;
+                    opt.aces -= 1;
                     //go over with another ace
                     if (i == 11 && opt.value + 11 > 21) {
                         opt.value += 1;
                     } else if (i == 11) {
                         opt.value += 11;
-                        opt.soft = true;
+                        opt.aces += 1;
                     //go over with something else
                     } else {
                         opt.value += i;
@@ -158,11 +269,10 @@ public class StrategyGenerator {
                     //went under
                     if (opt.value + 11 <= 21) {
                         opt.value += 11;
-                        opt.soft = true;
+                        opt.aces += 1;
                     //go over. Notice the hand must be hard here
                     } else if (opt.value + 11 > 21) {
                         opt.value += 1;
-                        opt.soft = false;
                     }
                 }
                 
